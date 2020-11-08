7a04538e7755481980d466625236b8040e8fc7cd
unknown <10414642+Roaders@users.noreply.github.com>
2020-11-08 10:03:06 +0000

    Export helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,6 @@ export * from './command-line.helper';
 export * from './content.helper';
 export * from './markdown.helper';
 export * from './visitor';
+export * from './line-ending.helper';
+export * from './options.helper';
+export * from './string.helper';
